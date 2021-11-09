beb85008c7baef335bd8d016779627d7d600ba29
Antwone83 <92768235+Antwone83@users.noreply.github.com>
2021-11-08 21:18:59 -0800

    Add files via upload

---
+++ Assignment 1.js
@@ -0,0 +1,24 @@
+console.log("Assignment 1")
+let userName="Antwone";
+let age="38";
+let geoLocation="Washington DC";
+let occupation="retire";
+let branch="United States Navy (Beat Army)";
+let residing="San Diego CA";
+let sportsTeam="Dallas Cowboys";
+let favHobby="Photography";
+let faavHobby="DJ";
+let civilianJob="Amazon Delivery Service Provider";
+let exercise="Cycling (Road Bikes)";
+let currentProfession="professional stay at home college student";
+let sportsPlayer="coaching volleyball";
+let animal="dog";
+let dogName="Hope";
+let secret="gadget nerd";
+let favFood="Shrimp,Tacos and Chicken with tator totts"
+let favDrink="flavored water";
+let wish="hit the lottery";
+
+document.write(` <h3> My name is ${userName}. I am ${age} years old, originally from ${geoLocation} but residing in ${residing}. After 20 years in the ${branch}, I decided to ${occupation} and work for ${civilianJob}. That job did not last long. No more manual labor for this guy so I became a ${currentProfession}. My body is needs the rest. In my spare time I like to  ${exercise} and watcg football (How Bout Dem ${sportsTeam}). One day soon I hope to get back into ${sportsPlayer} here in ${residing}. The best kept secret about me is that I am a ${secret} for ${faavHobby} and ${favHobby} equipment. My ${animal} named ${dogName} kept me entertained before going on vacation to ${geoLocation} to live with my mother. One other thing, I'm a foodie to heart. ${favFood} with a nice glass of ${favDrink} is my go to meal. One day I hope to ${wish} and travel the world.</h3>`)
+
+console.log(` Name: ${userName} \nAge: ${age}\ngeoLocation: ${geoLocation}\nresiding: ${residing}\nbranch: ${branch}\noccupation: ${occupation}\ncivilianJob: ${civilianJob}\ncurrentProfession: ${currentProfession}\nexercise: ${exercise}\nsportsTeam: ${sportsTeam}\nsportsPlayer: ${sportsPlayer}\nsecret: ${secret}\nfaavHobby: ${faavHobby}\nfavHobby: ${favHobby}\nanimal: ${animal}\ndogName: ${dogName}\nfavFood: ${favFood}\nfavDrink: ${favDrink}\nwish: ${wish}`)
